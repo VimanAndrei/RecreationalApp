b1d6d0f0d33c792f396bd39104772048abad5dfb
Viman Andrei <andrei.viman04@gmail.com>
2022-12-18 19:54:01 +0200

    add on long clik listener

--- app/src/main/java/com/example/myapplication/ActivityPrinter.kt
+++ app/src/main/java/com/example/myapplication/ActivityPrinter.kt
@@ -2,8 +2,13 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.PopupMenu
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 import com.example.myapplication.databinding.ListItemBinding
 
 class ActivityPrinter(): AppCompatActivity() {
@@ -15,13 +20,18 @@ class ActivityPrinter(): AppCompatActivity() {
         super.onCreate(savedInstanceState)
         var intent = this.intent
         var title = intent.getStringExtra("title")
-        var listOfActivity = intent.getSerializableExtra("list")
+        var listOfActivity = intent.getSerializableExtra("list") as ArrayList<OneActivity>
         setContentView(R.layout.list_item)
         binding = ListItemBinding.inflate(layoutInflater)
         setContentView(binding.root)
         findViewById<TextView>(R.id.pageTitle).text = title
 
+        binding.listView.adapter=ActivityAdapter(this, listOfActivity)
 
-        binding.listView.adapter=ActivityAdapter(this, listOfActivity as ArrayList<OneActivity>)
+        binding.listView.setOnItemLongClickListener { adapterView, view, i, l ->
+            Toast.makeText(applicationContext, "Long clicked on ${binding.listView[i]}", Toast.LENGTH_LONG).show()
+            PopupMenu(this,binding.listView)
+            true
+        }
     }
 }
